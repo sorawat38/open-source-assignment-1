d3dd13776c91c228071fd4120203fef98527145b
sorawat38 <st.sorawat38@gmail.com>
2025-03-18 23:37:19 -0600

    [fix] correct generic type in BorrowRecordRepository

--- src/main/java/org/example/assigment/repository/BorrowRecordRepository.java
+++ src/main/java/org/example/assigment/repository/BorrowRecordRepository.java
@@ -1,8 +1,9 @@
 package org.example.assigment.repository;
 
+import org.example.assigment.model.BorrowRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BorrowRecordRepository extends JpaRepository<BorrowRecordRepository, Long> {
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
 }
