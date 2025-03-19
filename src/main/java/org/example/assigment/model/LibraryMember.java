8ddd60d89c9e9de2be3944506b96f92f5dd6f7c9
sorawat38 <st.sorawat38@gmail.com>
2025-03-18 23:04:13 -0600

    [feat] make email be unique

--- src/main/java/org/example/assigment/model/LibraryMember.java
+++ src/main/java/org/example/assigment/model/LibraryMember.java
@@ -10,6 +10,7 @@ public class LibraryMember {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String phoneNumber;
 
